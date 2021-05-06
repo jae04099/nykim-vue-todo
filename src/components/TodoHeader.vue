143fa2e711caa919ad82d4612dcf1339a8cde97f
gyuri <jae04099@gmail.com>
2021-05-06 23:58:40 +0900

    nykimwork74

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="header">
+        <h1 class="logo">My Todo</h1>
+        <p class="date">{{timestamp}}</p>
+    </header>
+</template>
+<script>
+export default {
+    name: 'TodoHeader',
+    data(){
+        return{
+            timestamp: ""
+        }
+    }
+}
+</script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,8 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    name: 'TodoInput'
+}
+</script>
