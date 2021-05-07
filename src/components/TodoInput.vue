f8b53a9d44799b075635e673ea3bfd0f306c7cdf
Lena <jae04099@gmail.com>
2021-05-07 13:32:43 +0900

    styling

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,3 +4,13 @@
     <button class="add__button">Add</button>
 </div>
 </template>
+<style lang="scss">
+.add {
+  position: relative;
+  max-width: $max-width;
+  margin: 0 auto;
+  .main-input {
+    @include animation(fadeShow, 800ms, 1, 900ms);
+  }
+}
+</style>
